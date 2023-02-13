dcd82d0e937f33c0002165b7f8cb91c728fd8690
Andres <andres.solanov.est@uets.edu.ec>
2023-02-13 11:43:34 -0500

    primer commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['diseños.css']
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
 
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Desea seguir con el protocolo planteado',
+      message: 'This is an alert!',
+      buttons: ['OK','Cancel','Posponer'],
+    });
+
+    await alert.present();
+  }
 }
